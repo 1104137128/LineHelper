9d3368bfc27fba6957e48c97ee580ca03081e4c0
Vincent <vincent.liu@FootPrintKu.com>
2023-03-21 12:04:47 +0800

    feat: add login function

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,5 +1,20 @@
 export default function Login() {
+
+    const onSubmit = (ev) => {
+        ev.preventDefault;
+    }
+
     return (
-        <div>Login</div>
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Login into your account</h1>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+
+                    <button className="btn btn-block">Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
